7cbec76023c67278958c42495e0bc38f81850c60
Kai Yao <kai.b.yao@gmail.com>
2019-05-10 14:04:50 -0500

    moved DEFAULT parameters to the VALUES string (was previously in the parameter values array).

--- src/queries/insert_into_table.rs
+++ src/queries/insert_into_table.rs
@@ -11,12 +11,6 @@ use std::collections::HashMap;
 
 static INSERT_ROWS_BATCH_COUNT: usize = 2;
 
-// we are using this enum to denote whether an inserted row's column value is given (otherwise DEFAULT)
-enum ColumnTypeValueToInsert {
-    Default,
-    Value(ColumnTypeValue),
-}
-
 /// Runs an INSERT INTO <table> query
 pub fn insert_into_table(conn: &Connection, query: Query) -> Result<QueryResult, ApiError> {
     // extract query data
@@ -64,14 +58,12 @@ fn execute_insert<'a>(
     rows: &'a [Map<String, Value>],
     table: &str,
 ) -> Result<usize, ApiError> {
+    // TODO: figure out is_upsert
+
     // parse out the columns that have values to assign
     let columns = get_all_columns_to_insert(rows);
 
     dbg!(&columns);
-
-    // TODO: figure out is_upsert
-
-    // create initial prepared statement
 
     // OK, apparently serde_json::Values can't automatically convert to non-JSON/JSONB columns. We need to actually get column types of the table we're inserting into so we know what type to convert each value into.
     let column_stats = get_column_stats(conn, table)?;
@@ -82,6 +74,7 @@ fn execute_insert<'a>(
 
     let (values_params_str, column_values) = get_insert_params(rows, &columns, column_types)?;
 
+    // create initial prepared statement
     let insert_query_str = [
         "INSERT INTO ",
         table,
@@ -97,34 +90,31 @@ fn execute_insert<'a>(
 
     // convert the column values into the actual values we will use for the INSERT statement execution
     let mut prep_values: Vec<&ToSql> = vec![];
-    for column_type_value_to_insert in column_values.iter() {
-        match column_type_value_to_insert {
-            ColumnTypeValueToInsert::Default => prep_values.push(&"DEFAULT"),
-            ColumnTypeValueToInsert::Value(column_type_value) => match column_type_value {
-                ColumnTypeValue::BigInt(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::Bool(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::ByteA(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::Char(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::Citext(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::Date(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::Decimal(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::Float8(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::HStore(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::Int(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::Json(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::JsonB(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::MacAddr(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::Name(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::Oid(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::Real(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::SmallInt(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::Text(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::Time(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::Timestamp(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::TimestampTz(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::Uuid(col_val) => prep_values.push(col_val),
-                ColumnTypeValue::VarChar(col_val) => prep_values.push(col_val),
-            },
+    for column_value in column_values.iter() {
+        match column_value {
+            ColumnTypeValue::BigInt(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::Bool(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::ByteA(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::Char(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::Citext(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::Date(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::Decimal(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::Float8(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::HStore(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::Int(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::Json(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::JsonB(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::MacAddr(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::Name(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::Oid(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::Real(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::SmallInt(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::Text(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::Time(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::Timestamp(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::TimestampTz(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::Uuid(col_val) => prep_values.push(col_val),
+            ColumnTypeValue::VarChar(col_val) => prep_values.push(col_val),
         };
     }
 
@@ -160,53 +150,50 @@ fn get_insert_params(
     rows: &[Map<String, Value>],
     columns: &[&str],
     column_types: HashMap<String, String>,
-) -> Result<(String, Vec<ColumnTypeValueToInsert>), ApiError> {
+) -> Result<(String, Vec<ColumnTypeValue>), ApiError> {
     let mut prep_column_number = 1;
     let mut row_strs = vec![];
 
-    for row in rows {
-        let mut row_str_arr: Vec<String> = vec![];
-        for column in columns {
-            let prep_column_number_str = ["$", &prep_column_number.to_string()].join("");
-            row_str_arr.push(prep_column_number_str);
-            prep_column_number += 1;
-        }
-        row_strs.push(format!("({})", row_str_arr.join(", ")));
-    }
-
-    let values_str = row_strs.join(", ");
-
-    // create the vector of "values" query string (use DEFAULT for the columns that don't have a value in that row)
-
     // generate the array of json-converted-to-rust_postgres values to insert.
-    let nested_column_values_result: Result<Vec<Vec<ColumnTypeValueToInsert>>, ApiError> = rows
+    let nested_column_values_result: Result<Vec<Vec<ColumnTypeValue>>, ApiError> = rows
         .iter()
-        .map(|row| -> Result<Vec<ColumnTypeValueToInsert>, ApiError> {
-            columns
-                .iter()
-                .map(|column| match row.get(*column) {
+        .map(|row| -> Result<Vec<ColumnTypeValue>, ApiError> {
+            // row_str_arr is used for the prepared statement parameter string
+            let mut row_str_arr: Vec<String> = vec![];
+            let mut column_values: Vec<ColumnTypeValue> = vec![];
+
+            for column in columns.iter() {
+                // if the "row" json object has a value for column, then use the rust-converted value, otherwise use the column’s DEFAULT value
+                match row.get(*column) {
                     Some(val) => {
+                        let prep_column_number_str =
+                            ["$", &prep_column_number.to_string()].join("");
+                        row_str_arr.push(prep_column_number_str);
+                        prep_column_number += 1;
+
                         let column_type = &column_types[*column];
                         match ColumnTypeValue::from_json(column_type, val) {
                             Ok(column_type_value) => {
-                                Ok(ColumnTypeValueToInsert::Value(column_type_value))
+                                column_values.push(column_type_value);
                             }
-                            Err(e) => Err(e),
+                            Err(e) => return Err(e),
                         }
                     }
-                    None => Ok(ColumnTypeValueToInsert::Default),
-                })
-                .collect()
+                    None => {
+                        row_str_arr.push("DEFAULT".to_string());
+                    }
+                };
+            }
+
+            row_strs.push(format!("({})", row_str_arr.join(", ")));
+            Ok(column_values)
         })
         .collect();
-    let nested_column_values = match nested_column_values_result {
-        Ok(result) => result,
-        Err(e) => return Err(e),
-    };
 
-    // flatten!
-    let column_values: Vec<ColumnTypeValueToInsert> =
-        nested_column_values.into_iter().flatten().collect();
+    let values_str = row_strs.join(", ");
+
+    let nested_column_values = nested_column_values_result?;
+    let column_values: Vec<ColumnTypeValue> = nested_column_values.into_iter().flatten().collect();
 
     Ok((values_str, column_values))
 }
